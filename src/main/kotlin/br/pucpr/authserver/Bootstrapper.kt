de374f76dcf48c0cf173cb87e640bc2d476fb7cb
AndreMatioski02 <matioskiandre4@gmail.com>
2025-02-14 20:16:30 -0300

    Bootstrapper fix

--- src/main/kotlin/br/pucpr/authserver/Bootstrapper.kt
+++ src/main/kotlin/br/pucpr/authserver/Bootstrapper.kt
@@ -1,44 +1,62 @@
-//package br.pucpr.authserver
-//
-//import br.pucpr.authserver.levels.Level
-//import br.pucpr.authserver.levels.LevelRepository
-//import br.pucpr.authserver.roles.Role
-//import br.pucpr.authserver.roles.RoleRepository
-//import br.pucpr.authserver.rolesLevels.RoleLevel
-//import br.pucpr.authserver.rolesLevels.RoleLevelRepository
-//import br.pucpr.authserver.users.User
-//import br.pucpr.authserver.users.UserRepository
-//import org.springframework.context.ApplicationListener
-//import org.springframework.context.event.ContextRefreshedEvent
-//import org.springframework.data.repository.findByIdOrNull
-//import org.springframework.stereotype.Component
-//
-//@Component
-//class Bootstrapper(
-//    private val rolesRepository: RoleRepository,
-//    private val levelRepository: LevelRepository,
-//    private val roleLevelRepository: RoleLevelRepository,
-//    private val userRepository: UserRepository,
-//): ApplicationListener<ContextRefreshedEvent> {
-//    override fun onApplicationEvent(event: ContextRefreshedEvent) {
-//        val adminRole = rolesRepository.findByIdOrNull("ADMIN")
-//            ?: rolesRepository.save(Role("ADMIN", "System Administrator"))
-//
-//        val adminLevel = levelRepository.findByIdOrNull(1)
-//            ?: levelRepository.save(Level(1,"Sênior", "Master System Administrator"))
-//
-//        val adminRoleLevel = roleLevelRepository.findByIdOrNull(2)
-//            ?: roleLevelRepository.save(RoleLevel(2,adminRole, adminLevel))
-//
-//        if(userRepository.findByRoleLevel(2).isEmpty()) {
-//            val admin = User(
-//                email="amin@authserver.com",
-//                password = "admin",
-//                name = "Auth Server Administrator",
-//                roleLevel = adminRoleLevel
-//            )
-//
-//            userRepository.save(admin)
-//        }
-//    }
-//}
+package br.pucpr.authserver
+
+import br.pucpr.authserver.employee.Employee
+import br.pucpr.authserver.levels.Level
+import br.pucpr.authserver.levels.LevelRepository
+import br.pucpr.authserver.roles.Role
+import br.pucpr.authserver.roles.RoleRepository
+import br.pucpr.authserver.rolesLevels.RoleLevel
+import br.pucpr.authserver.rolesLevels.RoleLevelRepository
+import br.pucpr.authserver.employee.EmployeeRepository
+import org.springframework.context.ApplicationListener
+import org.springframework.context.event.ContextRefreshedEvent
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+
+@Component
+class Bootstrapper(
+    private val rolesRepository: RoleRepository,
+    private val levelRepository: LevelRepository,
+    private val roleLevelRepository: RoleLevelRepository,
+    private val employeeRepository: EmployeeRepository,
+): ApplicationListener<ContextRefreshedEvent> {
+    override fun onApplicationEvent(event: ContextRefreshedEvent) {
+        val role = Role(
+            name = "Desenvolvedor",
+            description = "Front-end"
+        )
+
+        val level = Level(
+            name = "Sênior",
+            description = "Large Experience"
+        )
+
+        val roleLevel = RoleLevel(
+            role = role,
+            level = level
+        )
+
+        if(rolesRepository.findByIdOrNull(1) == null) {
+            rolesRepository.save(role)
+        }
+
+        if(levelRepository.findByIdOrNull(1) == null) {
+            levelRepository.save(level)
+        }
+
+        if(roleLevelRepository.findByIdOrNull(1) == null) {
+            roleLevelRepository.save(roleLevel)
+        }
+
+        if(employeeRepository.findByRoleLevel(2).isEmpty()) {
+            val admin = Employee(
+                email="andre@email.com",
+                password = "admin",
+                name = "André Matioski",
+                roleLevel = roleLevel
+            )
+
+            employeeRepository.save(admin)
+        }
+    }
+}
